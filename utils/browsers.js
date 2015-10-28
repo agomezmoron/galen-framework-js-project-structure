6e5559ca13ab84f292d49ebe3c49074fa796ac4f
Alejandro Gómez Morón <amoron@emergya.com>
2015-10-28 23:21:17 +0100

    Adding more mobiles, phablets and tables

--- utils/browsers.js
+++ utils/browsers.js
@@ -29,6 +29,7 @@ var deviceTypes = {
 
 /**
  * All possible devices.
+ * They were extracted from http://mydevice.io/devices/
  */
 var devices = {
     // mobiles
@@ -45,11 +46,6 @@ var devices = {
     iphone6: {
         deviceName: "iphone6",
         size: "667x375",
-        tags: [deviceTypes.mobile]
-    },
-    iphone6Plus: {
-        deviceName: "iphone6Plus",
-        size: "736x414",
         tags: [deviceTypes.mobile]
     },
     LGG4:{
@@ -127,10 +123,276 @@ var devices = {
         size: "360x600",
         tags: [deviceTypes.mobile]
     },
-    // tables
-    ipadAir: {
-        deviceName: "ipadAir",
+    MicrosoftLumia1020:{
+        deviceName: "Microsoft Lumia 1020",
+        size: "320x480",
+        tags: [deviceTypes.mobile]
+    },
+    MicrosoftLumia925:{
+        deviceName: "Microsoft Lumia 925",
+        size: "320x480",
+        tags: [deviceTypes.mobile]
+    },
+    MicrosoftLumia920:{
+        deviceName: "Microsoft Lumia 920",
+        size: "320x480",
+        tags: [deviceTypes.mobile]
+    },
+    MicrosoftLumia920:{
+        deviceName: "Microsoft Lumia 920",
+        size: "320x480",
+        tags: [deviceTypes.mobile]
+    },
+    MicrosoftLumia830:{
+        deviceName: "Microsoft Lumia 830",
+        size: "320x480",
+        tags: [deviceTypes.mobile]
+    },
+    MicrosoftLumia620:{
+        deviceName: "Microsoft Lumia 620",
+        size: "320x480",
+        tags: [deviceTypes.mobile]
+    },
+    HTCOne:{
+        deviceName: "HTC One",
+        size: "360x640",
+        tags: [deviceTypes.mobile]
+    },
+    HTC8X:{
+        deviceName: "HTC 8X",
+        size: "320x480",
+        tags: [deviceTypes.mobile]
+    },
+    HTCEvo3D:{
+        deviceName: "HTC Evo 3D",
+        size: "360x640",
+        tags: [deviceTypes.mobile]
+    },
+    SonyXperiaZ3:{
+        deviceName: "Sony Xperia Z3",
+        size: "360x598",
+        tags: [deviceTypes.mobile]
+    },
+    SonyXperiaZ:{
+        deviceName: "Sony Xperia Z",
+        size: "360x640",
+        tags: [deviceTypes.mobile]
+    },
+    SonyXperiaS:{
+        deviceName: "Sony Xperia S",
+        size: "360x640",
+        tags: [deviceTypes.mobile]
+    },
+    SonyXperiaP:{
+        deviceName: "Sony Xperia P",
+        size: "360x640",
+        tags: [deviceTypes.mobile]
+    },
+    XiaomiMi3:{
+        deviceName: "Xiaomi Mi 3",
+        size: "360x640",
+        tags: [deviceTypes.mobile]
+    },
+    XiaomiMi4:{
+        deviceName: "Xiaomi Mi 4",
+        size: "360x640",
+        tags: [deviceTypes.mobile]
+    },
+    Lenovo K900:{
+        deviceName: "Lenovo K900",
+        size: "360x640",
+        tags: [deviceTypes.mobile]
+    },
+    BlackberryLeap:{
+        deviceName: "Blackberry Leap",
+        size: "390x695",
+        tags: [deviceTypes.mobile]
+    },
+    BlackberryPassport:{
+        deviceName: "Blackberry Passport",
+        size: "504x504",
+        tags: [deviceTypes.mobile]
+    },
+    BlackberryClassic:{
+        deviceName: "Blackberry Classic",
+        size: "390x390",
+        tags: [deviceTypes.mobile]
+    },
+    BlackberryQ10:{
+        deviceName: "Blackberry Q10",
+        size: "346x346",
+        tags: [deviceTypes.mobile]
+    },
+    BlackberryZ30:{
+        deviceName: "Blackberry Z30",
+        size: "360x640",
+        tags: [deviceTypes.mobile]
+    },
+    BlackberryZ10:{
+        deviceName: "Blackberry Z10",
+        size: "384x640",
+        tags: [deviceTypes.mobile]
+    },
+    BlackberryTorch:{
+        deviceName: "Blackberry Torch",
+        size: "480x360",
+        tags: [deviceTypes.mobile]
+    },
+    ZTEGrandS:{
+        deviceName: "ZTE Grand S",
+        size: "360x640",
+        tags: [deviceTypes.mobile]
+    },
+    ZTEOpen:{
+        deviceName: "ZTE Open",
+        size: "320x480",
+        tags: [deviceTypes.mobile]
+    },
+    // phablets
+    iphone6Plus: {
+        deviceName: "iphone6Plus",
+        size: "736x414",
+        tags: [deviceTypes.phablet]
+    },
+    MotorolaNexus6: {
+        deviceName: "Motorola Nexus 6",
+        size: "412x690",
+        tags: [deviceTypes.phablet]
+    },
+    MotorolaNexus6: {
+        deviceName: "Microsoft Lumia 1520",
+        size: "432x768",
+        tags: [deviceTypes.phablet]
+    },
+    SamsungGalaxyNote3: {
+        deviceName: "Samsung Galaxy Note 3",
+        size: "360x640",
+        tags: [deviceTypes.phablet]
+    },
+    SamsungGalaxyNote4: {
+        deviceName: "Samsung Galaxy Note 4",
+        size: "360x640",
+        tags: [deviceTypes.phablet]
+    },
+    // tablets
+    ipadPro: {
+        deviceName: "Apple iPad Pro",
+        size: "1024x1366",
+        tags: [deviceTypes.tablet]
+    },
+    ipad1: {
+        deviceName: "Apple iPad 1",
         size: "768x1024",
+        tags: [deviceTypes.tablet]
+    },
+    ipad2: {
+        deviceName: "Apple iPad 2",
+        size: "768x1024",
+        tags: [deviceTypes.tablet]
+    },
+    ipad3: {
+        deviceName: "Apple iPad 4",
+        size: "768x1024",
+        tags: [deviceTypes.tablet]
+    },
+    ipad4: {
+        deviceName: "Apple iPad 4",
+        size: "768x1024",
+        tags: [deviceTypes.tablet]
+    },
+    ipadAir: {
+        deviceName: "Apple iPad Air",
+        size: "768x1024",
+        tags: [deviceTypes.tablet]
+    },
+    ipadMini: {
+        deviceName: "Apple iPad mini",
+        size: "768x1024",
+        tags: [deviceTypes.tablet]
+    },
+    ipadMini2: {
+        deviceName: "Apple iPad mini 2",
+        size: "768x1024",
+        tags: [deviceTypes.tablet]
+    },
+    ipadMini3: {
+        deviceName: "Apple iPad mini 3",
+        size: "768x1024",
+        tags: [deviceTypes.tablet]
+    },
+    SamsungGalaxyTab_8_9inch: {
+        deviceName: "Samsung Galaxy Tab 8.9''",
+        size: "800x1280",
+        tags: [deviceTypes.tablet]
+    },
+    SamsungGalaxyTab2_10inch: {
+        deviceName: "Samsung Galaxy Tab 2 10''",
+        size: "800x1280",
+        tags: [deviceTypes.tablet]
+    },
+    SamsungGalaxyTab2_7inch: {
+        deviceName: "Samsung Galaxy Tab 2 7''",
+        size: "600x1024",
+        tags: [deviceTypes.tablet]
+    },
+    SamsungGalaxyTab3_10inch: {
+        deviceName: "Samsung Galaxy Tab 3 10''",
+        size: "800x1280",
+        tags: [deviceTypes.tablet]
+    },
+    SamsungNexus10: {
+        deviceName: "Samsung Nexus 10",
+        size: "800x1280",
+        tags: [deviceTypes.tablet]
+    },
+    HTCNexus9: {
+        deviceName: "HTC Nexus 9",
+        size: "768x1024",
+        tags: [deviceTypes.tablet]
+    },
+    AsusNexus7V1: {
+        deviceName: "Asus Nexus 7 (v1)",
+        size: "600x960",
+        tags: [deviceTypes.tablet]
+    },
+    AsusNexus7V2: {
+        deviceName: "Asus Nexus 7 (v2)",
+        size: "604x966",
+        tags: [deviceTypes.tablet]
+    },
+    AmazonKindleFireHD8_9: {
+        deviceName: "Amazon Kindle Fire HD 8.9",
+        size: "600x960",
+        tags: [deviceTypes.tablet]
+    },
+    AmazonKindleFireHD7: {
+        deviceName: "Amazon Kindle Fire HD 7",
+        size: "480x800",
+        tags: [deviceTypes.tablet]
+    },
+    MicrosoftSurface: {
+        deviceName: "Microsoft Surface",
+        size: "768x1366",
+        tags: [deviceTypes.tablet]
+    },
+    MicrosoftSurfacePro: {
+        deviceName: "Microsoft Surface Pro",
+        size: "720x1280",
+        tags: [deviceTypes.tablet]
+    },
+    MicrosoftSurfacePro2: {
+        deviceName: "Microsoft Surface Pro 2",
+        size: "720x1280",
+        tags: [deviceTypes.tablet]
+    },
+    MicrosoftSurfacePro3: {
+        deviceName: "Microsoft Surface Pro 3",
+        size: "1024x1440",
+        tags: [deviceTypes.tablet]
+    },
+    BlackberryPlaybook: {
+        deviceName: "Blackberry Playbook",
+        size: "600x1024",
         tags: [deviceTypes.tablet]
     },
     // desktops
